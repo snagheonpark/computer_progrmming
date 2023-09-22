b7bc05ce2ea5cf28e7dd35ce37e967643e0f9081
snagheonpark <144418509+snagheonpark@users.noreply.github.com>
2023-09-22 14:31:17 +0900

    간단한 실습
    실습

---
+++ 간단한 실습.py
@@ -0,0 +1,8 @@
+a = input("나이를 입력하세요: ")
+
+age = int(a)
+
+if age > 19:
+    print("성인입니다.")
+else:
+    print("미성년자입니다.")
---
+++ 제어문.py
@@ -0,0 +1,9 @@
+a = 10
+b = 3
+
+if a + b < 14 :
+    print("yeah, it is true.")
+
+content = "감자"
+if content == "고구마"
+    print("그것은 고구마이다.")
