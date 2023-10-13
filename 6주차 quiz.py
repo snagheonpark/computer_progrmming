7a05bf10bdc9b46d5ca708ace5decd200006e75c
snagheonpark <144418509+snagheonpark@users.noreply.github.com>
2023-10-13 15:47:35 +0900

    Create 6주차 quiz.py
    퀴즈 제출

---
+++ 6주차 quiz.py
@@ -0,0 +1,26 @@
+class 붕어빵:
+    def __init__(self, type, price):
+        self.type = type
+        self.price = price
+        self.total = 0
+
+    def sell(self):
+        print(self.type + f"붕어빵을 {self.price}에 팔았습니다.")
+        self.total += self.price
+
+
+    def eat(self):
+        print(self.type + "붕어빵을 먹었습니다.")
+
+슈크림 = 붕어빵("슈크림", 700)
+
+팥 = 붕어빵("팥", 500)
+
+슈크림.sell()
+슈크림.sell()
+슈크림.eat()
+
+팥.sell()
+팥.eat()
+
+print(슈크림.total + 팥.total)
